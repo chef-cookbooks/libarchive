bc1a7adc9db1211415f246f4aefae74e970fe9d2
Nick Shemonsky <nicholas.shemonsky@synchronoss.com>
2015-09-15 12:23:41 -0400

    rubocop / foodcritic

--- providers/file.rb
+++ providers/file.rb
@@ -9,14 +9,14 @@ require 'fileutils'
 
 action :extract do
   unless ::File.exist?(new_resource.path)
-    raise Errno::ENOENT, "no archive found at #{new_resource.path}"
+    fail Errno::ENOENT, "no archive found at #{new_resource.path}"
   end
 
   FileUtils.mkdir_p(new_resource.extract_to, mode: new_resource.mode)
 
   Chef::Log.info "libarchive_file[#{new_resource.name}] extracting #{new_resource.path} to #{new_resource.extract_to}"
   updated = LibArchiveCookbook::Helper.extract(new_resource.path, new_resource.extract_to,
-    Array(new_resource.extract_options))
+                                               Array(new_resource.extract_options))
 
   if new_resource.owner || new_resource.group
     FileUtils.chown_R(new_resource.owner, new_resource.group, new_resource.extract_to)
